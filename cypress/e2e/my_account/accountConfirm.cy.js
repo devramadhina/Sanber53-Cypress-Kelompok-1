ebf6dafbad75496c5fba5205502f1b8b058fd441
ichirizqi <ichrzq94@gmail.com>
2024-02-03 11:49:16 +0700

    Edit Account Information Negative Case

--- cypress/e2e/my_account/accountConfirm.cy.js
+++ cypress/e2e/my_account/accountConfirm.cy.js
@@ -7,9 +7,35 @@ describe('Edit Account Information', () => {
   })
 
   it('Success Edit Account Information', () => {
-    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+    editAccount.clickEdit()
     cy.get(editAccount.firstname).clear().type(editData["valid-data"]["first-name"])
     cy.get(editAccount.lastname).clear().type(editData["valid-data"]["last-name"])
     editAccount.clickSave()
+    cy.url().should('eq', 'https://magento.softwaretestingboard.com/customer/account/')
+  })
+
+  it.only('Failed Edit Account Information - Blank All Input Field', () => {
+    editAccount.clickEdit()
+    cy.get(editAccount.firstname).clear()
+    cy.get(editAccount.lastname).clear()
+    editAccount.clickSave()
+    editAccount.verifyErrFirst()
+    editAccount.verifyErrLast()
+  })
+
+  it('Failed Edit Account Information - Blank Fisrtname', () => {
+    editAccount.clickEdit()
+    cy.get(editAccount.firstname).clear()
+    cy.get(editAccount.lastname).clear().type(editData["valid-data"]["last-name"])
+    editAccount.clickSave()
+    editAccount.verifyErrFirst()
+  })
+
+  it('Failed Edit Account Information - Blank LastName', () => {
+    editAccount.clickEdit()
+    cy.get(editAccount.firstname).clear().type(editData["valid-data"]["first-name"])
+    cy.get(editAccount.lastname).clear()
+    editAccount.clickSave()
+    editAccount.verifyErrLast()
   })
 })
