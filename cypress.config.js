cd1075dc97bbe691a1e56865ca5a30042f2f5f45
devramadhina <ramadhinatrieananda@gmail.com>
2024-02-07 02:29:56 +0700

    add credential at config

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,10 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     env: {
-      email: "ichiiirs@gmail.com",
-      pwd: "IchiSanber=123"
+      email1: "ichiiirs@gmail.com",
+      pwd: "IchiSanber=123",
+      email2: "ramadhinatrieananda@gmail.com",
+      pwd: "Ramadhina@123"
     },
     defaultCommandTimeout: 5432,
     viewportHeight: 650,
